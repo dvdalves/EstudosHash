cd2385e0e4eabeb34989ca377afc3d4af24f0d00
David Alves <david.a0595@gmail.com>
2024-08-21 23:14:32 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EstudosHash;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+
+        var sha256 = new Sha256();
+        Sha256.GetHash(input);
+    }
+}
---
+++ Sha256.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace EstudosHash;
+
+public class Sha256
+{
+    public static string GetHash(string input)
+    {
+        var hash = new StringBuilder();
+
+        using (var sha256Hash = SHA256.Create())
+        {
+            var crypto = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+        }
+
+        string hashString = hash.ToString();
+        Console.WriteLine("Hash: {0}", hashString);
+
+        return hashString;
+    }
+}
